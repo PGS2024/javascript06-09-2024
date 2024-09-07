ee98bcc34c3f0022b42d983cc3d109e85b534956
Paulo GS2024 <164101438+PGS2024@users.noreply.github.com>
2024-09-06 21:25:48 -0300

    Add files via upload

---
+++ geovana.js
@@ -0,0 +1,5 @@
+
+let nome ="Geovana";
+let idade = 21;
+alert(nome + " tem " + idade + " anos.");
+// Geovana tem 21 anos 
---
+++ index.js
@@ -0,0 +1,13 @@
+let numeroInteiro = parseInt("3.5")
+console.log(numeroInteiro)
+
+
+let vinte = Number(prompt("Digite 20 e pressione Enter: "));
+console.log(vinte + 1); //O resultado será 21 
+
+let nome = "Maria Eduarda";
+let idade = 20;
+// Maria Eduarda 
+console.log(nome, "tem" , idade ,"anos.");
+// Maria Eduarda tem 20 anos
+
